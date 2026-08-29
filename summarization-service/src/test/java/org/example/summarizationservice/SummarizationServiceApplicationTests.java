1755d89d34af5d9148019d1f16f7c842004a968f
LinkerM <105450527+LinkerMak@users.noreply.github.com>
2026-08-29 16:08:33 +0400

    commit 21. build(summarization-service). added summarization-service module

---
+++ summarization-service/src/test/java/org/example/summarizationservice/SummarizationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.summarizationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SummarizationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
